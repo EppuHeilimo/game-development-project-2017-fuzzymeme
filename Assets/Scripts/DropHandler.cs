1a0bb6df320774b6038247ec5d70eef48df491e5
Matze94 <matthias.schmidinger@students.fhv.at>
2017-04-03 09:37:00 +0300

    Enemy Item Drop
    The DropHandler, attached to the enemy itself, now is able to drop the
    test weapon at the location of the enemy, the script is attached to.
    To use it i think, we need to execute this methods, when the enemy is
    dying. Maybe we should discuss this together :)

--- Assets/Scripts/DropHandler.cs
+++ Assets/Scripts/DropHandler.cs
@@ -10,29 +10,48 @@ using UnityEngine;
 
 public class DropHandler : MonoBehaviour {
 
-    public double DropChance = 0.5; // Has to be a value between 0.0 and 1.0
+    public double DropChance = 1.0; // Has to be a value between 0.0 and 1.0
     private List<GameObject> differentWeapons; // List of all the weapon prefabs 
     private GameObject test;  // test value
 
 
     /**
      * First function to be executed!
-     * Gets the list of all weapon prefabs used  in the project.
-     * 
-     * TODO: Needs to be extended as soon as we have prefabs
-     * and an idea about the droprates
+     * Gets the list of all weapon prefabs used  in the project. 
      **/
     private void Start()
     {
+        differentWeapons = new List<GameObject>();
+        /*
         GameObject obj = GameObject.FindGameObjectWithTag("GameManager");
         GameManager gm = obj.GetComponent<GameManager>();
         differentWeapons = gm.weapons;
-        test = differentWeapons[0];
+        test = differentWeapons[0];     */
+        
+        UnityEngine.Object[] loadedweapons = Resources.LoadAll("Weapons");
+        foreach (UnityEngine.Object weapon in loadedweapons)
+        {
+            differentWeapons.Add((GameObject)weapon);
+        }  
+
+
+        foreach (GameObject obj in differentWeapons)
+        {
+            Debug.Log(obj);
+        }
+        createDropLoot(differentWeapons[0]);
+      
+
+
+
     }
 
     /**
      * Using the droprates this function should be possible if or what item will be dropped.
      * Call the createDropLoot - method with the right game object to be dropped
+     * 
+     * TODO: Needs to be extended as soon as we have prefabs
+     * and an idea about the droprates
      **/
     public void calculateDropRates() 
     {
@@ -46,11 +65,22 @@ public class DropHandler : MonoBehaviour {
      }
 
     /**
+     * This method will return the current location the current enemy is standing at.
+     * This will guarantee, the loot will be dropped at the same location the enemy has been killed.
+     **/ 
+    private Transform getSpawnLocation()
+    {
+        Transform spawnLocation = this.transform;
+        return spawnLocation;
+    }
+
+    /**
      * Give different parameters for different prefabs to be created
      **/
     private void createDropLoot(GameObject prefab) 
     {
-        var itemToDrop = (GameObject)Instantiate(prefab, new Vector3(0f, 0f, 0f), new Quaternion() ) ;
+        Transform spawnLocation = getSpawnLocation();
+        var itemToDrop = (GameObject)Instantiate(prefab, spawnLocation.position, spawnLocation.rotation);
     }
 
     /**
